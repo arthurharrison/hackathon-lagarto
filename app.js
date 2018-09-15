f3678972fd576ee3b252b38e8edbb635e4554429
arthurharrison <tavares593@gmail.com>
2018-09-15 02:54:56 -0300

    Advances in the chat

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const app = require('http').createServer(response);
 const fs = require('fs');
+const io = require('socket.io')(app)
 
 app.listen(3030);
 console.log("App is running on port 3030...");
@@ -19,4 +20,24 @@ function response(req, res){
         res.writeHead(200);
         res.end(data);
     });
+  }
+
+  io.on("connection", (socket) => {
+    io.emit('chat message', ' a new user has joined');
+  
+    socket.on("send message", (sent_msg, callback) => {
+        sent_msg = "[" + getCurrentDate() + "]: " + sent_msg;
+        io.sockets.emit("update messages", sent_msg);
+        callback();
+    });
+  
+    socket.on('disconnect', function() {
+         io.emit('chat message', 'some user disconnected');
+      });
+  });
+  function getCurrentDate() {
+      const currentDate = new Date();
+      const hour = (currentDate.getHours() < 10 ? '0' : '') + currentDate.getHours();
+      const minute = (currentDate.getMinutes() < 10 ? '0' : '') + currentDate.getMinutes();
+      return  hour + ":" + minute;
   }
