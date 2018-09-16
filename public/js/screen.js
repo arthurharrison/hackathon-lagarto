059f2dd6164045188ee77147446d257fdbe98141
arthur harrison <tavares593@gmail.com>
2018-09-15 21:07:59 -0300

    Fixes in the structure and texts in the main page

--- public/js/screen.js
+++ public/js/screen.js
@@ -1,11 +1,10 @@
 document.getElementById("btnGo").onclick = () => {
     const name = document.getElementById('msgName').value;
     const cns = document.getElementById('msgCns').value;
-    const tel = (document.getElementById('msgPs').value != null ? document.getElementById('msgPs').value : 'x' );
+    //const ps = (document.getElementById('msgPs').value != null ? document.getElementById('msgPs').value : 'x' );
     //const email = (document.getElementById('msgEmail').value != null ? document.getElementById('msgEmail').value : 'x' );
     document.getElementById('msgName').value = ''
-    document.getElementById('msgCid').value = ''
-    document.getElementById('msgPs').value = ''
+    document.getElementById('msgCns').value = ''
     document.getElementById('loginIn').style.display = 'none';
     document.getElementById('chatter').style.display = 'block';
     $.ajax({
