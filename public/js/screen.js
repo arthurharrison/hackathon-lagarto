e5675ca248c183792db7860d9ae62202e119619f
arthur harrison <tavares593@gmail.com>
2018-09-16 00:20:52 -0300

    Fixes in the Chat and changed some colors

--- public/js/screen.js
+++ public/js/screen.js
@@ -1,3 +1,5 @@
+let socket = null;
+
 document.getElementById("btnGo").onclick = () => {
     const name = document.getElementById('msgName').value;
     const cns = document.getElementById('msgCns').value;
@@ -12,9 +14,30 @@ document.getElementById("btnGo").onclick = () => {
         method:'POST',
         data: {name: name}
       }).done(function(){
+        socket = io.connect();
+        
+$("form#chat").submit(function(e) {
+  e.preventDefault();
+  socket.emit("send message",  $(this).find("#msg_text").val(), function() {
+      $("form#chat #msg_text").val("");
+  });
+});
+
+socket.on('chat message', function(msg){
+  //handle the message however you would like
+  $('.cHistory').append($('<p />').text(msg));
+
+});
+
+socket.on("update messages", function(msg){
+  var final_message = $("<p />").text(msg);
+  $(".cHistory").append(final_message);
+});
+   
         console.log('deu certo');
       }).fail(function(){
         console.log('deu ruim');
       });
 
 }
+
