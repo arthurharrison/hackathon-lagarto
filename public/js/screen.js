ce343965aeb57e80f1ec51bcfcc37f169ff77ec0
arthur harrison <tavares593@gmail.com>
2018-09-16 03:22:26 -0300

    Work in the Home of the Page

--- public/js/screen.js
+++ public/js/screen.js
@@ -16,23 +16,23 @@ document.getElementById("btnGo").onclick = () => {
       }).done(function(){
         socket = io.connect();
         
-$("form#chat").submit(function(e) {
-  e.preventDefault();
-  socket.emit("send message",  $(this).find("#msg_text").val(), function() {
-      $("form#chat #msg_text").val("");
-  });
-});
+        $("form#chat").submit(function(e) {
+          e.preventDefault();
+          socket.emit("send message",  $(this).find("#msg_text").val(), function() {
+              $("form#chat #msg_text").val("");
+          });
+        });
 
-socket.on('chat message', function(msg){
-  //handle the message however you would like
-  $('.cHistory').append($('<p />').text(msg));
+        socket.on('chat message', function(msg){
+          //handle the message however you would like
+          $('.cHistory').append($('<p />').text(msg));
 
-});
+        });
 
-socket.on("update messages", function(msg){
-  var final_message = $("<p />").text(msg);
-  $(".cHistory").append(final_message);
-});
+        socket.on("update messages", function(msg){
+          var final_message = $("<p />").text(msg);
+          $(".cHistory").append(final_message);
+        });
    
         console.log('deu certo');
       }).fail(function(){
